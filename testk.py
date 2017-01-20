6dfd1caffa43af40464698441756f0bae230bcb2
Zander Moss <zander@mit.edu>
2017-01-19 19:56:37 -0500

    Commented the testing script. Need to comment the code itself.

--- testk.py
+++ testk.py
@@ -2,11 +2,12 @@
 import recursive_covariance as rc
 import numpy as np
 
-x=2.0
+xlow=2.0
+xhigh=4.0
 alpha=2
 beta=3
 l=4
-n=4
+n=3
 
 #Generate a dictionary of Xn. This will fill as Xn(x) are requested.
 #These calls are also idempotent, so no values are calculated twice.	
@@ -23,6 +24,10 @@ kca = rc.KCrawlerArray(xca)
 #objects contain dictionaries which store trees in (l,n) indices, as
 #well as methods to crawl down these trees and fill in the nodes without
 #redundant calculation.
-print kca.GetEntry((x,alpha,beta),(l,n))
 
-		
+#Calculating at two x values and comparing the difference to a numerical
+#integral in mathematica over the [xlow,xhigh] interval.
+
+ylow = kca.GetEntry((xlow,alpha,beta),(l,n))
+yhigh = kca.GetEntry((xhigh,alpha,beta),(l,n))
+print "Integral: ",yhigh-ylow
