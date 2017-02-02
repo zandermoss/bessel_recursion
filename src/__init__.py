6cadbdd80cbb23ddcf696ed3ec4d4ccd6e694284
Zander Moss <zander@mit.edu>
2017-02-02 13:04:27 -0500

    I was dumb, realized that the src directory gives module name, switched back to Crawlers/. Updated the Doxyfile

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from Crawler import Crawler, CrawlerDict
-from XYCrawler import XYCrawler, XYCrawlerDict
-from ICrawler import ICrawler, ICrawlerDict
-from KCrawler import KCrawler, KCrawlerDict
-from HCrawler import HCrawler, HCrawlerDict
